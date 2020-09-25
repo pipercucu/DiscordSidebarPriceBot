3090693021743c7e0a2ac577196f5b0d90603114
Piper <pipercucurbit@gmail.com>
2020-09-25 12:41:37 -0400

    Gas and USD, ETH, and BTC price rotation.

---
+++ batchstart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+nohup node index.js BTC 60000 0 &
+nohup node index.js ETH 60000 1 1 &
+nohup node index.js UNISWAP 60000 2 &
+nohup node index.js SWRV 120000 3 &
+nohup node index.js "HOTPOT BASE TOKEN" 60000 4 &
+nohup node index.js YFI 120000 5 1 &
+nohup node index.js AGI 120000 6 1 &
+nohup node index.js PAXG 120000 7 1 &
+nohup node index.js SNX 120000 8 1 &
+nohup node index.js DOT 120000 9 &
+nohup node index.js ⛽ 60000 10 &
